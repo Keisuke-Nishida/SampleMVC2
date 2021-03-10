ab773cac7d93e01f405e51a0e79a4514fecdb3df
keisuke.nishida <keisuke.nishida@frontarc.co.jp>
2021-03-10 17:14:26 +0900

    first commit

---
+++ SampleMVC2/Data/SampleMVC2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SampleMVC2.Models;
+
+
+namespace SampleMVC2.Data
+{
+    public class SampleMVC2Context : DbContext
+    {
+        public SampleMVC2Context(DbContextOptions<SampleMVC2Context> options)
+            : base(options)
+        {
+        }
+
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
